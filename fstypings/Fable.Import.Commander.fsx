24b605e5dd95e9bb396b6d1eff6b7cd2ec03d9ec
jbtule <jay+code@tuley.name>
2016-07-30 16:04:23 -0500

    intial import

---
+++ fstypings/Fable.Import.Commander.fsx
@@ -0,0 +1,74 @@
+namespace Fable.Import
+#r "../node_modules/fable-core/Fable.Core.dll"
+open System
+open System.Text.RegularExpressions
+open Fable.Core
+open Fable.Import.Node
+open Fable.Import.JS
+
+module commander =
+    type [<AllowNullLiteral>] ICommandStatic =
+        [<Emit("new $0($1...)")>] abstract Create: ?name: string -> ICommand
+
+    and ICommand =
+        inherit NodeJS.EventEmitter
+        abstract args: ResizeArray<string> with get, set
+        abstract _args: ResizeArray<obj> with get, set
+        abstract command: name: string * ?desc: string * ?opts: obj -> ICommand
+        abstract addImplicitHelpCommand: unit -> unit
+        abstract parseExpectedArgs: args: ResizeArray<string> -> ICommand
+        abstract action: fn: Func<obj, unit> -> ICommand
+        abstract option: flags: string * ?description: string * ?fn: U2<Func<obj, obj, unit>, Regex> * ?defaultValue: obj -> ICommand
+        abstract option: flags: string * ?description: string -> ICommand
+        abstract allowUnknownOption: ?arg: bool -> ICommand
+        abstract parse: argv: ResizeArray<string> -> ICommand
+        abstract executeSubCommand: argv: ResizeArray<string> * args: ResizeArray<string> * unknown: ResizeArray<string> -> obj
+        abstract normalize: args: ResizeArray<string> -> ResizeArray<string>
+        abstract parseArgs: args: ResizeArray<string> * unknown: ResizeArray<string> -> ICommand
+        abstract optionFor: arg: string -> IOption
+        abstract parseOptions: argv: ResizeArray<string> -> obj
+        abstract opts: unit -> obj
+        abstract missingArgument: name: string -> unit
+        abstract optionMissingArgument: option: obj * ?flag: string -> unit
+        abstract unknownOption: flag: string -> unit
+        abstract version: str: string * ?flags: string -> ICommand
+        abstract description: str: string -> ICommand
+        abstract description: unit -> string
+        abstract alias: alias: string -> ICommand
+        abstract alias: unit -> string
+        abstract usage: str: string -> ICommand
+        abstract usage: unit -> string
+        abstract name: unit -> string
+        abstract largestOptionLength: unit -> float
+        abstract optionHelp: unit -> string
+        abstract commandHelp: unit -> string
+        abstract helpInformation: unit -> string
+        abstract outputHelp: unit -> unit
+        abstract help: unit -> unit
+
+    and [<AllowNullLiteral>] IOptionStatic =
+        [<Emit("new $0($1...)")>] abstract Create: flags: string * ?description: string -> IOption
+
+    and [<AllowNullLiteral>] IOption =
+        abstract flags: string with get, set
+        abstract required: bool with get, set
+        abstract optional: bool with get, set
+        abstract bool: bool with get, set
+        abstract short: string option with get, set
+        abstract long: string with get, set
+        abstract description: string with get, set
+        abstract name: unit -> string
+        abstract is: arg: string -> bool
+
+    and IExportedCommand =
+        inherit ICommand
+        abstract Command: ICommandStatic with get, set
+        abstract Option: IOptionStatic with get, set
+        //custom fields
+        abstract download:bool with get
+        abstract count:bool with get
+
+    type [<Import("*","commander")>] Globals =
+        static member ``default`` with get(): IExportedCommand = failwith "JS only" and set(v: IExportedCommand): unit = failwith "JS only"
+
+
---
+++ main.fsx
@@ -0,0 +1,81 @@
+module Run
+#r "node_modules/fable-core/Fable.Core.dll"
+#load "fstypings/Fable.Import.Commander.fsx"
+
+open System
+open Fable.Core
+open Fable.Import
+open Fable.Import.Node
+open Fable.Import.commander
+
+// This just shows how a F# Console Application can be
+// translated to node.js. The entry point we'll be called automatically
+// with the arguments passed to the node script.
+// Note that for this simple example no dependencies but fable-core are needed.
+
+[<EntryPoint>]
+let main argv =
+    let nodeArgs = ResizeArray(argv)
+    nodeArgs.Insert(0,"poltiface-honstey.js")
+    nodeArgs.Insert(0,"node")
+
+    let program = Fable.Import.commander.Globals.``default``
+
+    program
+        .version("1.0.0")
+        .option("-d, --download", "Download data from Politifact")
+        .parse(nodeArgs)
+        |> ignore
+
+    let people = [ //people used in the Robert Mann Graph
+                    "donald-trump"
+                    "michele-bachmann"
+                    "ted-cruz"
+                    "newt-gingrich"
+                    "sarah-palin"
+                    "rick-santorum"
+                    "scott-walker"
+                    "rick-perry"
+                    "paul-ryan"
+                    "john-mccain"
+                    "mitt-romney"
+                    "rand-paul"
+                    "chris-christie"
+                    "joe-biden"
+                    "john-kasich"
+                    "bernie-s"
+                    "jeb-bush"
+                    "hillary-clinton"
+                    "barack-obama"
+
+                  ]
+
+    if program.download then
+        let downloadFile url filename =
+            let path = sprintf "./data/%s" filename
+            let file = fs.createWriteStream(path)
+            printfn "Downloading %s..." path
+            http.get( url, Func<_,_>(fun r-> 
+                                        r.pipe(file) |> ignore
+                                        printfn "Downloaded %s." path
+                                    ))
+
+        let downloadPerson personSlug =
+                let numberOfComments = 75 //bug in poltifact api includes subjects as well of speakers, going up to get 50
+                let url = sprintf "http://www.politifact.com/api/statements/truth-o-meter/people/%s/json/?n=%i" personSlug numberOfComments
+                let fileName = sprintf "people/%s.json" personSlug
+                downloadFile url fileName
+
+        let afterMkDirHandler _ =
+            downloadFile  "http://www.politifact.com/api/people/all/json/" "people.json"
+                                    |> ignore
+            let statuses = people 
+                            |> Seq.map downloadPerson
+                            |> Seq.toList
+            ()
+
+        fs.mkdir("data/people", Func<_,_>(afterMkDirHandler))
+        0
+    else if program.count then
+        1    else
+        1
