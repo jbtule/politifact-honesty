ffca847789bd4a2a3be74f477d62b49b52f84b16
jbtule <jay+code@tuley.name>
2016-08-09 23:33:05 -0500

    Oops, forgot to filter statements about candidate, data matching up

--- src/count.fsx
+++ src/count.fsx
@@ -6,10 +6,24 @@ open Fable.Core
 open Fable.Import.Node
 
 let run (peopleSlugs:string list) =
+
+  let rulingSlugs = [
+      "pants-fire"
+      "false"
+      "barely-true"
+      "half-true"
+      "mostly-true"
+      "true"
+  ]
+  
+  let rulingSlugsSet = Set(rulingSlugs)
+
   let countIt (personSlug:string) (statements: Politifact.Statement[]) =
     statements
       |> Array.filter (fun s-> s.speaker.name_slug = personSlug)
+      |> Array.filter (fun s->rulingSlugsSet.Contains(s.ruling.ruling_slug))
       |> Array.length
+
   let parseIt person =
     let fileName = sprintf "data/people/%s.json" person
     let json = fs.readFileSync(fileName).toString()
@@ -21,33 +35,28 @@ let run (peopleSlugs:string list) =
      peopleSlugs
       |> List.map parseIt
   
-  let slugs = [
-      "pants-fire"
-      "false"
-      "barely-true"
-      "half-true"
-      "mostly-true"
-      "true"
-  ]
+
 
   let writeData limit =
     printfn "Generating csv for %i" limit
     let file = sprintf "data/people/%i.csv" limit
-    fs.writeFileSync(file,"0.0",null)
-    fs.appendFileSync(file,String.Join(",","name"::slugs),null)
+    fs.writeFileSync(file,"",null)
+    fs.appendFileSync(file,String.Join(",","name"::rulingSlugs),null)
     fs.appendFileSync(file, "\n" ,null)
 
-    for slug,statements,count in parsed do
+    for personSlug,statements,count in parsed do
       if count >= limit then
         let latestByLimit =
           statements
+            |> Array.filter (fun s-> s.speaker.name_slug = personSlug)
+            |> Array.filter (fun s-> rulingSlugsSet.Contains(s.ruling.ruling_slug))
             |> Array.sortBy (fun x-> x.statement_date)
             |> Array.rev
             |> Array.take limit
             |> Array.countBy (fun x -> x.ruling.ruling_slug)
           
         let counts = 
-          slugs
+          rulingSlugs
             |> List.map (fun x-> 
                               let found = 
                                 latestByLimit 
@@ -57,9 +66,9 @@ let run (peopleSlugs:string list) =
                                 | None -> "0"
                         )
 
-        fs.appendFileSync(file, String.Join(",",slug::counts) ,null)
+        fs.appendFileSync(file, String.Join(",",personSlug::counts) ,null)
         fs.appendFileSync(file, "\n" ,null)
-        printfn "%s:%i" slug count
+        printfn "%s:%i" personSlug count
 
   writeData 50
   writeData 100
